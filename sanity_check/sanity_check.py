4980d9a64d3380056d6c51d57a7b0b3eb980a061
Xiaoyi Qu <xiq322@lehigh.edu>
2025-05-13 22:30:25 -0400

    Add sanity check file

--- sanity_check/sanity_check.py
+++ sanity_check/sanity_check.py
@@ -8,19 +8,20 @@ sys.path.append(parentdir)
 """
 Quantization test cases
 """
-# from test_qmlp import TestQMLP
-# from test_qvgg7bn import TestQVGG7BN
-# from test_qresnet18 import TestQResNet18
-# from test_qresnet20 import TestQResNet20
-# from test_qresnet56 import TestQResNet56
-# from test_qresnet50 import TestQResNet50
-# from test_qbert import TestQBert
-# from test_qcarn import TestQCARN
-# from test_qyolov5 import TestQYolov5
-# from test_qsimplevit import TestQSimpleViT
-# from test_qphi2 import TestQPhi2
-# from test_qdeit import TestQDeiT
-from test_qswin import TestQSwin
+from test_qmlp import TestQMLP
+from test_qvgg7bn import TestQVGG7BN
+from test_qresnet18 import TestQResNet18
+from test_qresnet20 import TestQResNet20
+from test_qresnet56 import TestQResNet56
+from test_qresnet50 import TestQResNet50
+from test_qbert import TestQBert
+from test_qcarn import TestQCARN
+from test_qyolov5 import TestQYolov5
+from test_qsimplevit import TestQSimpleViT
+from test_qphi2 import TestQPhi2
+from test_qvit import TestQViT
+from test_qdeit import TestQDeiT
+# from test_qswin import TestQSwin
 
 # from test_simplevit import TestSimpleViT
 # from test_vit import TestViT
